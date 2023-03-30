1f316eb64771f41c5fed45677fb0bc3469328012
Davina Phan <dphan@email.neit.edu>
2023-03-30 10:58:03 -0400

    level1.1 completed

---
+++ gds137/level1/js/week1_basic_movement.js
@@ -0,0 +1,30 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	var ball = new Ball();
+	
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	//Move the Player
+	ball.x += 0;
+	
+	//Update the Screen
+	ball.draw();
+}
