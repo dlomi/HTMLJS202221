7db48598aadd3bc67b6929ac8062b109b163c4ef
Davina Phan <dphan@email.neit.edu>
2023-04-13 09:46:21 -0400

    completed 2.6

---
+++ gds137/level2/lvl2-6/js/GameObject.js
@@ -0,0 +1,113 @@
+// JavaScript Document
+
+var player1 = new GameObject();
+var ball = new GameObject();
+
+ball.x = canvas.width/2;
+ball.y = canvas.height/2;
+
+ball.width = 100;
+ball.height = 100;
+
+
+
+function GameObject(x,y,w,h,color)
+{
+	
+	//Default Values
+	if(x == undefined)
+		this.x = canvas.width/2;
+	else 
+		this.x = x;
+	if(y == undefined)
+		this.y = canvas.height/2;
+	else 
+		this.y = y;
+	
+	if(w == undefined)
+		this.width = 100;
+	else 
+		this.width = w;
+	if(h == undefined)
+		this.height = 100;
+	else 
+		this.height = h;
+	
+		//player's color
+	if(color == undefined)
+		this.color = "#89Cff0";
+	else 
+		this.color = color;
+	
+	//player's velocity or speed on each axis
+	this.vx = 0;
+	this.vy = 0;
+
+
+	
+
+	
+	//This draws the player to the screen
+	this.drawRect = function()
+	{
+		context.save();
+			context.fillStyle = this.color;
+			context.translate(this.x = canvas.width/200, this.y);
+			context.fillRect((-this.width/2), (-this.height/2), this.width = 30, this.height = 140);
+		context.restore();
+		
+	}	
+	
+	this.drawCircle = function()
+	{
+		context.save();
+			context.fillStyle = "#800080";
+			context.beginPath();
+			context.translate(this.x, this.y);
+			context.arc(0, 0, this.width/2, 0, 360 *Math.PI/180, true);
+			context.arc(0, 0, this.width/2, 0, 360 *Math.PI/180, true);
+			context.closePath();
+			context.fill();
+		context.restore();
+		
+	}	
+	
+	//This changes the player's position
+	this.move = function()
+	{
+		this.x += this.vx;
+		this.y += this.vy;
+	}
+	
+	this.left = function() 
+	{
+		return this.x - this.width/2;
+	}
+	this.right = function() 
+	{
+		return this.x + this.width/2;
+	}
+	
+	this.top = function() 
+	{
+		return this.y - this.height/2;
+	}
+	this.bottom = function() 
+	{
+		return this.y + this.height/2;
+	}
+	
+	this.hitTestObject = function(obj)
+	{
+		if(this.left() < obj.right() && 
+		   this.right() > obj.left() &&
+		   this.top() < obj.bottom() &&
+		   this.bottom() > obj.top())
+		{
+			return true;
+			
+		}
+		return false;
+	}
+	
+}
---
+++ gds137/level2/lvl2-6/js/week2_keyboard_movement.js
@@ -0,0 +1,107 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+
+
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new GameObject();
+	player.x = 100;
+
+
+	ball.vx = -4;
+	ball.vy = 0;
+
+	ball.height = 50;
+	ball.width = 50;
+
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	ball.x += ball.vx;
+	ball.y += ball.vy;
+
+	if(w)
+	{
+		console.log("Moving Up");
+		player.y -= 4;
+	}
+	if(s)
+	{
+		console.log("Moving Down");
+		player.y += 4;
+	}
+
+	if(player.y < player.height/2)
+	{
+		player.y = player.height/2
+	}
+	if(player.y > canvas.height - player.height/2)
+	{
+		player.y = canvas.height - player.height/2
+	}
+	if(ball.x < -ball.width/2)
+	{
+		ball.x = canvas.width/2
+		ball.vx = 4;
+	}
+	if(ball.x > canvas.width - ball.width/2)
+	{
+		ball.x = canvas.width - ball.width/2
+		ball.vx = -ball.vx;
+	
+
+	}
+	if(ball.y < ball.height/2)
+	{
+		ball.y = ball.height/2
+		ball.vy = -ball.vy;
+		ball.vy = 10;
+
+	}
+	if (ball.y > canvas.height - ball.height/2)
+	{
+		ball.y = canvas.height - ball.height/2
+		ball.vy = -ball.vy;
+
+	}
+
+
+
+	if(player.hitTestObject(ball))
+	{
+		ball.x = player.x + player.width/2 + ball.width/2
+		ball.vx = 4;
+		if(ball.y < player.y - player.height/6)
+		{
+			ball.vy = -4;
+		}
+		if(ball.y > player.y + player.height/6)
+		{
+			ball.vy = 4;
+		}
+	}
+	
+
+
+
+	//Update the Screen
+
+	player.drawRect();
+	ball.drawCircle();
+}
+
