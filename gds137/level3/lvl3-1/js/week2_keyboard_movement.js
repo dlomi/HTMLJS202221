164e13670a60cb865ec0c2a44471d28f14e42c53
Davina Phan <dphan@email.neit.edu>
2023-04-20 10:44:15 -0400

    started 3.1

---
+++ gds137/level3/lvl3-1/js/week2_keyboard_movement.js
@@ -0,0 +1,175 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+var playerTwo;
+
+
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new GameObject();
+	player.x = 100;
+	playerTwo = new GameObject();
+
+	
+	
+	ball.vx = -4;
+	ball.vy = 0;
+
+	ball.height = 50;
+	ball.width = 50;
+
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	ball.x += ball.vx;
+	ball.y += ball.vy;
+
+	if(w)
+	{
+		console.log("Moving Up");
+		player.y -= 4;
+	}
+	if(s)
+	{
+		console.log("Moving Down");
+		player.y += 4;
+	}
+	if(a)
+	{
+		console.log("moving up")
+		playerTwo.y -= 4;
+	}
+
+
+
+	if(player.y < player.height/2)
+	{
+		player.y = player.height/2
+	}
+	if(player.y > canvas.height - player.height/2)
+	{
+		player.y = canvas.height - player.height/2
+	}
+
+	if(playerTwo.y < player.height/2)
+	{
+		playerTwo.y = playerTwo.height/2
+	}
+	if(playerTwo.y > canvas.height - playerTwo.height/2)
+	{
+		playerTwo.y = canvas.height - playerTwo.height/2
+	}
+
+	//left boundary of canvas
+	if(ball.x < -ball.width/2)
+	{
+		ball.x = canvas.width/2
+		ball.vx = 4;
+	}
+	//right boundary of canvas
+	if(ball.x > canvas.width - ball.width/2)
+	{
+		ball.x = canvas.width/2
+		ball.y = canvas.height/2
+		
+	}
+	if(ball.y < ball.height/2)
+	{
+		ball.y = ball.height/2
+		ball.vy = -ball.vy;
+		ball.vy = 10;
+
+	}
+	if (ball.y > canvas.height - ball.height/2)
+	{
+		ball.y = canvas.height - ball.height/2
+		ball.vy = -ball.vy;
+
+	}
+
+	if (player.hitTestObject(ball))
+	{
+		if(ball.y < player.y - 30)
+		{
+			ball.vx = 4;
+			ball.vy = -4;
+		}
+		if(ball.y < player.y - 60)
+		{
+			ball.vx = -ball.vx;
+		}
+		if(ball.y < player.y - 150)
+		{
+			ball.vx = 4;
+			ball.vy = -4;
+		}
+	}
+	if (playerTwo.hitTestObject(ball))
+	{
+		if(ball.y < playerTwo.y - 30)
+		{
+			ball.vx = 4;
+			ball.vy = -4;
+		}
+		if(ball.y < playerTwo.y - 60)
+		{
+			ball.vx = -ball.vx;
+		}
+		if(ball.y < playerTwo.y - 150)
+		{
+			ball.vx = 4;
+			ball.vy = -4;
+		}
+	}
+
+
+	if(player.hitTestObject(ball))
+	{
+		ball.x = player.x + player.width/2 + ball.width/2
+		ball.vx = 4;
+		if(ball.y < player.y - player.height/6)
+		{
+			ball.vy = -4;
+		}
+		if(ball.y > player.y + player.height/6)
+		{
+			ball.vy = 4;
+		}
+	}
+	if(playerTwo.hitTestObject(ball))
+	{
+		ball.x = playerTwo.x + playerTwo.width/2 + ball.width/2
+		ball.vx = 4;
+		if(ball.y < playerTwo.y - playerTwo.height/6)
+		{
+			ball.vy = -4;
+		}
+		if(ball.y > playerTwo.y + playerTwo.height/6)
+		{
+			ball.vy = 4;
+		}
+	}
+
+
+
+	//Update the Screen
+
+	player.drawRect();
+	playerTwo.drawRect();
+	ball.drawCircle();
+}
+
